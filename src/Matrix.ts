b3c9636d2b51446d45da976aff91d3c09183a218
Николай <demna2014@yandex.ru>
2021-09-03 20:13:34 +0300

    Add matrix proportions.

--- src/Matrix.ts
+++ src/Matrix.ts
@@ -1,6 +1,11 @@
 export class Matrix<T> {
 
+    public readonly N: number;
+    public readonly M: number;
     constructor(
         private readonly matrix: T[][],
-    ) { }
+    ) {
+        this.N = matrix.length;
+        this.M = Math.min(...matrix.map(row => row.length));
+     }
 }
