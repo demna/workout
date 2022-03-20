c5dbe0426100a22da95f745acab9600f6fbd0aac
DemidkovNA <demna2014@yandex.ru>
2022-03-20 18:11:35 +0300

    Add webpack

--- src/ArrayGenerator.ts
+++ src/ArrayGenerator.ts
@@ -6,4 +6,12 @@ export class ArrayGenerator {
         }
         return arr;
     }
+
+    static N(n: number): number[] {
+        const arr = [];
+        for (let i = 0; i < n; i++) {
+            arr.push(i+1);         
+        }
+        return arr;
+    }
 }
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
